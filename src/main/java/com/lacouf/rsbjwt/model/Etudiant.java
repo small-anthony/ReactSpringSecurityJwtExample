de10599f6692f84cb710fa81a697e199ac7e3ae2
C0de-V <valentin.dimitrov276@gmail.com>
2026-09-12 10:51:09 -0400

    Ajout du modèle étudiant

--- src/main/java/com/lacouf/rsbjwt/model/Etudiant.java
+++ src/main/java/com/lacouf/rsbjwt/model/Etudiant.java
@@ -1,10 +1,28 @@
 package com.lacouf.rsbjwt.model;
 
+import com.lacouf.rsbjwt.model.auth.Credentials;
 import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
 
 @Entity
 @DiscriminatorValue("S")
 public class Etudiant extends UserApp {
+    private int matricule;
+    private String discipline;
 
+    public Etudiant(String firstName, String lastName, Credentials credentials, int matricule, String discipline) {
+        super( firstName, lastName, credentials);
+        this.matricule = matricule;
+        this.discipline = discipline;
+    }
+
+    public Etudiant () {}
+
+    public int getMatricule() {
+        return matricule;
+    }
+
+    public String getDiscipline() {
+        return discipline;
+    }
 }
