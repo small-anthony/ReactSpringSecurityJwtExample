83b6befa50544fc25b57735506f9992d9439e09c
C0de-V <valentin.dimitrov276@gmail.com>
2026-09-12 10:29:57 -0400

    Modification pour que les record prennent un id sous forme de int et non de Long,

--- src/main/java/com/lacouf/rsbjwt/service/dto/ProfesseurDto.java
+++ src/main/java/com/lacouf/rsbjwt/service/dto/ProfesseurDto.java
@@ -4,11 +4,11 @@ import com.lacouf.rsbjwt.model.Professeur;
 import com.lacouf.rsbjwt.model.auth.Role;
 import com.lacouf.rsbjwt.service.dto.interfaceDTO.UserDto;
 
-public record ProfesseurDto(Long id, String firstName, String lastName, String email, Role role) implements UserDto {
+public record ProfesseurDto(int id, String firstName, String lastName, String email, Role role) implements UserDto {
 
     public static ProfesseurDto create(Professeur professeur) {
         return new ProfesseurDto(
-                professeur.getId(),
+                professeur.getId().intValue(),
                 professeur.getFirstName(),
                 professeur.getLastName(),
                 professeur.getEmail(),
@@ -17,6 +17,6 @@ public record ProfesseurDto(Long id, String firstName, String lastName, String e
     }
 
     public static ProfesseurDto empty() {
-        return new ProfesseurDto(null, null, null, null, null);
+        return new ProfesseurDto(0, null, null, null, null);
     }
 }
