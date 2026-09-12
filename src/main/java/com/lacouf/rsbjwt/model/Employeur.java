40aa2cca78d3d131c0fc61bf93a691574b623ab4
TheLewandoski8 <d302515167@gmail.com>
2026-09-12 11:31:32 -0400

    Ajout de la couche modele pour la classe Employeur

--- src/main/java/com/lacouf/rsbjwt/model/Employeur.java
+++ src/main/java/com/lacouf/rsbjwt/model/Employeur.java
@@ -1,10 +1,36 @@
 package com.lacouf.rsbjwt.model;
 
+import com.lacouf.rsbjwt.model.auth.Credentials;
 import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
 
 @Entity
 @DiscriminatorValue("E")
 public class Employeur extends UserApp {
+    private String entreprise;
+    private String posteOccupe;
+    private String telephone;
 
+    public Employeur() {
+        super();
+    }
+
+    public Employeur(String firstName, String lastName, Credentials credentials, String entreprise, String posteOccupe, String telephone) {
+        super(firstName, lastName, credentials);
+        this.entreprise = entreprise;
+        this.posteOccupe = posteOccupe;
+        this.telephone = telephone;
+    }
+
+    public String getEntreprise() {
+        return entreprise;
+    }
+
+    public String getPosteOccupe() {
+        return posteOccupe;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
 }
