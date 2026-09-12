3c06b99c9f0051479b3aa0f4ba0c34aaae9df15a
C0de-V <valentin.dimitrov276@gmail.com>
2026-09-12 12:50:15 -0400

    Ajout de l'enregistrement d'un étudiant dans le controleur

--- src/main/java/com/lacouf/rsbjwt/presentation/SignUpController.java
+++ src/main/java/com/lacouf/rsbjwt/presentation/SignUpController.java
@@ -1,9 +1,11 @@
 package com.lacouf.rsbjwt.presentation;
 
 import com.lacouf.rsbjwt.service.UserAppService;
+import com.lacouf.rsbjwt.service.dto.EtudiantDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -17,8 +19,23 @@ class SignUpController {
 	}
 
 	@PostMapping("/etudiant")
-	public ResponseEntity<Object> signUpEtudiant() {
-		return ResponseEntity.status(HttpStatus.NOT_IMPLEMENTED).build();
+	public ResponseEntity<Object> signUpEtudiant(
+			@RequestParam String firstName,
+			@RequestParam String lastName,
+			@RequestParam int matricule,
+			@RequestParam String email,
+			@RequestParam String discipline,
+			@RequestParam String password,
+			@RequestParam String confirmPassword) throws Exception {
+
+		try {
+			EtudiantDto etudiant = userService.registerEtudiant(firstName, lastName, matricule, email, discipline, password, confirmPassword);
+
+			return ResponseEntity.status(HttpStatus.CREATED).body(etudiant);
+
+		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+		}
 	}
 
 	@PostMapping("/gestionnaire")
