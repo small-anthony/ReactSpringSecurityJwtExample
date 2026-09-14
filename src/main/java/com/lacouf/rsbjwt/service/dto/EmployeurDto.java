c713eea945b9d6ab9311f83070af7dd3898448a9
TheLewandoski08 <161066947+TheLewandoski08@users.noreply.github.com>
2026-09-14 11:59:16 -0400

    Revert "Implementation du code pour enregistrer un nouveau employeur (#6)"
    This reverts commit a039d3d968dda169cffbb0e1592b2ba4da3de423.

--- src/main/java/com/lacouf/rsbjwt/service/dto/EmployeurDto.java
+++ src/main/java/com/lacouf/rsbjwt/service/dto/EmployeurDto.java
@@ -4,10 +4,7 @@ import com.lacouf.rsbjwt.model.Employeur;
 import com.lacouf.rsbjwt.model.auth.Role;
 import com.lacouf.rsbjwt.service.dto.interfaceDTO.UserDto;
 
-public record EmployeurDto(int id, String firstName,
-                           String lastName, String email,
-                           Role role, String entreprise,
-                           String telephone) implements UserDto {
+public record EmployeurDto(int id, String firstName, String lastName, String email, Role role) implements UserDto {
 
     public static EmployeurDto create(Employeur employeur) {
         return new EmployeurDto(
@@ -15,13 +12,11 @@ public record EmployeurDto(int id, String firstName,
                 employeur.getFirstName(),
                 employeur.getLastName(),
                 employeur.getEmail(),
-                employeur.getRole(),
-                employeur.getEntreprise(),
-                employeur.getTelephone()
+                employeur.getRole()
         );
     }
 
     public static EmployeurDto empty() {
-        return new EmployeurDto(0, null, null, null, null, null, null);
+        return new EmployeurDto(0, null, null, null, null);
     }
 }
