4247aba5c2b0f15f4e00730f3c8e9d985574074e
TheLewandoski08 <161066947+TheLewandoski08@users.noreply.github.com>
2026-09-15 09:08:52 -0400

    Revert "Implementation du code pour enregistrer un nouveau employeur (#6)" (#7)
    This reverts commit a039d3d968dda169cffbb0e1592b2ba4da3de423.

--- src/main/java/com/lacouf/rsbjwt/ReactSpringSecurityJwtApplication.java
+++ src/main/java/com/lacouf/rsbjwt/ReactSpringSecurityJwtApplication.java
@@ -1,18 +1,15 @@
 package com.lacouf.rsbjwt;
 
 import com.lacouf.rsbjwt.repository.*;
-import com.lacouf.rsbjwt.service.UserAppService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ReactSpringSecurityJwtApplication implements CommandLineRunner {
-    private final UserAppService userAppService;
     private final UserAppRepository userAppRepository;
 
-    public ReactSpringSecurityJwtApplication(UserAppService userAppService, UserAppRepository userAppRepository){
-        this.userAppService = userAppService;
+    public ReactSpringSecurityJwtApplication(UserAppRepository userAppRepository ){
         this.userAppRepository = userAppRepository;
     }
 
@@ -22,14 +19,5 @@ public class ReactSpringSecurityJwtApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        IO.println(userAppService.registerEmployeur(
-                "Valentin",
-                "Lacouf",
-                "valentin@gmail.com",
-                "McDonalds",
-                "514 111 1111",
-                "valentin",
-                "valentin"
-        ));
     }
 }
