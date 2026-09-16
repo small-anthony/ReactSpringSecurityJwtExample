48332804e57144296a6d67b1f2a803e11fe1cc28
small-anthony <small.anthony@hotmail.com>
2026-09-16 12:28:30 -0400

    Refactor Frontend pour preparation travail FE (#9)

--- react-vite-project/src/components/page/GestionnaireHome.jsx
+++ react-vite-project/src/components/page/GestionnaireHome.jsx
@@ -1,31 +1,7 @@
-import { useState } from "react";
-import fetcher from "../../utils/fetcher.js";
-
 const GestionnaireHome = () => {
-  const [message, setMessage] = useState("");
-
-  const handleAccessGestionnaireEndpoint = () => {
-    setMessage("");
-    fetcher("/user/gestionnaire/demo", { method: "GET" })
-      .then(async (response) => {
-        if (!response.ok) {
-          throw new Error(`Erreur API (${response.status})`);
-        }
-        const data = await response.text();
-        setMessage(data);
-      })
-      .catch((error) => {
-        setMessage(error.message);
-      });
-  };
-
   return(
     <>
       <h1>Page accueil gestionnaire</h1>
-      <button style={{ width: 'fit-content' }} onClick={handleAccessGestionnaireEndpoint}>
-        Accéder à l'endpoint gestionnaire
-      </button>
-      {message && <p>{message}</p>}
     </>
   );
 }
