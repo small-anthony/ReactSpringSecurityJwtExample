11d57547674c7586bd4cd28387597c53417b7b27
tonty <39193182+tontyGH@users.noreply.github.com>
2026-09-16 15:54:54 -0400

    Implement rough side-panel

--- react-vite-project/src/components/PageLayout.jsx
+++ react-vite-project/src/components/PageLayout.jsx
@@ -1,11 +1,19 @@
 import {Outlet} from "react-router-dom";
 import Header from "./Header.jsx";
 import './PageLayout.css'
+import SidePanel from "./widget/SidePanel.tsx";
 function PageLayout ({user}) {
   return (
-    <div id="pagelayout" className="pageLayout">
-      <Header user={user}/>
-      <Outlet />
+    <div id="pagelayout" className="pageLayout flex flex-col">
+        <Header user={user}/>
+        <div className={"flex-1 grid grid-flow-row grid-cols-12"}>
+            <div className={"col-span-2"}>
+                <SidePanel />
+            </div>
+            <div className={"col-span-10"}>
+                <Outlet/>
+            </div>
+        </div>
     </div>
   );
 }
--- react-vite-project/src/components/page/HomePage.jsx
+++ react-vite-project/src/components/page/HomePage.jsx
@@ -1,31 +1,11 @@
 import {useTranslation} from "react-i18next";
+import SidePanel from "../widget/SidePanel.tsx";
 
 const HomePage = ({user, options}) => {
     const t = useTranslation(["main"]);
 
-    function GenerateExampleButton(text) {
-        return (
-            <div className={""}>
-                {text}
-            </div>
-        );
-    }
-
     return (
         <div className={"flex flex-col flex-1"}>
-            <div className={"flex flex-1"}>
-                <div className={"flex flex-col flex-1/6"}>
-                    <div className={"flex-1/2 flex flex-col"} style={{backgroundColor: "red"}}>
-                        {GenerateExampleButton("Mes offres")}
-                    </div>
-                    <hr/>
-                    <div className={"flex-1/2"} style={{backgroundColor: "teal"}}>
-                        <h1 className={"text-center"}>Aucune notification</h1>
-                    </div>
-                </div>
-                <div className={"flex-5/6"}>
-                </div>
-            </div>
         </div>
     );
 }
