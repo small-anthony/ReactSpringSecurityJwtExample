250b2bd356c99d748eadf34aad4b030cbeba945f
C0de-V <valentin.dimitrov276@gmail.com>
2026-09-19 16:44:36 -0400

    Ajout des tests unitaires pour le controller

--- src/test/java/com/lacouf/rsbjwt/presentation/EmployeurControllerTest.java
+++ src/test/java/com/lacouf/rsbjwt/presentation/EmployeurControllerTest.java
@@ -1,7 +1,10 @@
 package com.lacouf.rsbjwt.presentation;
 
+import com.lacouf.rsbjwt.model.OffreStage;
+import com.lacouf.rsbjwt.service.EmployeurService;
 import com.lacouf.rsbjwt.service.UserAppService;
 import com.lacouf.rsbjwt.service.dto.EmployeurDto;
+import com.lacouf.rsbjwt.service.dto.OffreStageDto;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -9,6 +12,8 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+
 import java.util.HashMap;
 import java.util.Map;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -19,6 +24,9 @@ import static org.mockito.Mockito.when;
 public class EmployeurControllerTest {
     @Mock
     private UserAppService userAppService;
+
+    @Mock
+    private EmployeurService employeurService;
 
     @InjectMocks
     private EmployeurController employeurController;
@@ -84,4 +92,61 @@ public class EmployeurControllerTest {
         assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
         assertEquals("Le format du courriel n'est pas valide", response.getBody());
     }
+
+
+//    TEST TELEVERSEMENT OFFRE D'OFFRE DEMPLOI
+
+
+
+
+    @Test
+    void testCreerOffre_succes() throws Exception {
+//        ARRANGE
+        Map<String, String> request = new HashMap<>();
+        request.put("titre", "Stagiaire en informatique");
+        request.put("nomEntreprise", "CGI");
+        request.put("description", "Développement web React et Spring Boot");
+
+        Authentication authentication = mock(Authentication.class);
+        when(authentication.getName()).thenReturn("employeur@entreprise.com");
+
+
+        OffreStageDto offreDto = new OffreStageDto(1, "Stagiaire en informatique", "Développement web React et Spring Boot", "CGI");
+
+        when(employeurService.createOffreStage("Stagiaire en informatique", "CGI", "Développement web React et Spring Boot", "employeur@entreprise.com"))
+                .thenReturn(offreDto);
+
+//        ACT
+        ResponseEntity<Object> response = employeurController.creerOffre(request, authentication);
+
+//        ASSERT
+        assertEquals(HttpStatus.CREATED, response.getStatusCode());
+        assertEquals(offreDto, response.getBody());
+    }
+
+    @Test
+    void testCreerOffre_erreur_retourneBadRequest() throws Exception {
+
+//      ARRANGE
+        Map<String, String> request = new HashMap<>();
+        request.put("titre", "");
+        request.put("nomEntreprise", "CGI");
+        request.put("description", "Développement web React et Spring Boot");
+
+        Authentication authentication = mock(Authentication.class);
+        when(authentication.getName()).thenReturn("employeur@entreprise.com");
+
+        when(employeurService.createOffreStage("", "CGI", "Développement web React et Spring Boot", "employeur@entreprise.com"))
+                .thenThrow(new Exception("Le titre est obligatoire"));
+
+//        ACT
+        ResponseEntity<Object> response = employeurController.creerOffre(request, authentication);
+
+//        ASSERT
+        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+        assertEquals("Le titre est obligatoire", response.getBody());
+    }
+
+
+
 }
