e35d44d7f29dbb6c28d1c6394976c2ac3bf86176
TheLewandoski8 <d302515167@gmail.com>
2026-09-19 12:59:56 -0400

    Modification du code de Cv pour respecter la loi de Demeter

--- src/main/java/com/lacouf/rsbjwt/repository/CvRepository.java
+++
@@ -1,7 +0,0 @@
-package com.lacouf.rsbjwt.repository;
-
-import com.lacouf.rsbjwt.model.Cv;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CvRepository extends JpaRepository<Cv, Long> {
-}
