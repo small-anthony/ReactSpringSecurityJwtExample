324b1f790f8765307fe397e364cff582486bcfe5
tonty <39193182+tontyGH@users.noreply.github.com>
2026-09-19 11:42:25 -0400

    Implement login tests

--- src/main/java/com/lacouf/rsbjwt/security/exception/BadCredentialsException.java
+++ src/main/java/com/lacouf/rsbjwt/security/exception/BadCredentialsException.java
@@ -1,6 +1,10 @@
 package com.lacouf.rsbjwt.security.exception;
 
 public class BadCredentialsException extends AuthenticationException {
+	public BadCredentialsException() {
+		super("bad credentials");
+	}
+
 	public BadCredentialsException(String message) {
 		super(message);
 	}
